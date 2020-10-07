e7b0b298c32127a926f2822166ac36496c388a0c
ekoteneva <ekoteneva@bim-info.ru>
2020-10-07 10:41:39 +0300

    add test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+    const [value, setValue] = useState()
+     const increment = () => {
+        setCounter(prev => prev + 1)
+    }
+     const decrement = () => {
+      counter > 0 ? setCounter(prev => prev - 1) : alert('число не должно быть отрицательным')
+    }
+    const handleChange = (event) => {
+     setValue(event.target.value)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+       counter > 0 ? setCounter(value) : alert('число не должно быть отрицательным')
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Счётчик {counter}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+        <br />
+          <form onSubmit={handleSubmit} >
+              <input id="input" type="number" placeholder="Введите число" onChange={handleChange}/> <button type="submit">установить значение</button>
+          </form>
     </div>
   );
 }
 
 export default App;
+
+App.propTypes = {
+  count: PropTypes.number,
+  value: PropTypes.number
+};
